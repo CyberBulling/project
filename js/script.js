526538de040a7796740e747e3d4e5c5023999445
cyberbulling <98632404+CyberBulling@users.noreply.github.com>
2024-06-01 14:43:44 +0000

    pre-last commit, with no color gamma

--- js/script.js
+++ js/script.js
@@ -1,25 +1,39 @@
-let mainWindow,pauseWindow,canvas,scoreWindow,leaderboard,settings,restart,context, scoreInput;
-let velocity=0,acceleration=0.2,score=0,birdX,birdY, scoreTmp=0, firstPipeScore, alsoPipeScore, tagName="Player 1";
-let pipeWidth=80,pipeGap=120,firstPipeX=900,secondPipeX=1200,thirdPipeX=1500,fourPipeX=1800,firstPipeY,secondPipeY,thirdPipeY,fourPipeY,pipeSpeed=2;//change gap and speed for difficulty level
-let leaderboardMassive=[];
+let mainWindow,pauseWindow,canvas,scoreWindow,leaderButton,settings,restart,context, scoreInput,leaderboard, helloWindow,settings2,leaderButton2,restart2;
+let velocity=0,acceleration=0.2,score=0,birdX,birdY, scoreTmp=0, firstPipeScore, alsoPipeScore;
+let pipeWidth=80,pipeGap=120,pipeXGap=300,firstPipeX,secondPipeX,thirdPipeX,fourPipeX,firstPipeY,secondPipeY,thirdPipeY,fourPipeY,pipeSpeed=2;
+let struct=[],tagName="Player 1";
 const bird=new Image(),pipeTop=new Image(),pipeBottom=new Image(), backgroundImage=new Image();
-const approach=-7;//change for difficulty
+let approach=-7;
 bird.src='/img/bird.png';
-backgroundImage.src='/img/background-day-big.jpg';
+
+if(localStorage.getItem('theme')=='light'){
+    backgroundImage.src='/img/background-day-big.jpg';
+}
+else{
+    backgroundImage.src='/img/background-night-big.jpg';
+}
 pipeTop.src='/img/pipeTop.png';
 pipeBottom.src='/img/pipeBottom.png';
 
 window.onload = function(){
     mainWindow =document.getElementById('main');
     pauseWindow=document.getElementById('endGame');
-    scoreWindow=document.getElementById('score');
-    scoreInput=document.getElementById('scoreInput');
-    leaderboard=document.getElementById('leaderboard');
+    scoreInput=document.getElementById('score');
+    leaderButton=document.getElementById('leaderboard');
+    leaderboard=document.getElementById('leaders');
+    settingsWindow=document.getElementById('settingsWindow');
     settings=document.getElementById('settings');
     restart=document.getElementById('restart');
+    helloWindow=document.getElementById('hello');
+    settings2=document.getElementById('settings2');
+    leaderButton2=document.getElementById('leaderboard2');
+    restart2=document.getElementById('restart2');
     canvas=document.getElementById('canvas');
     context=canvas.getContext('2d');
-
+    firstPipeX=canvas.width;
+    secondPipeX=canvas.width+pipeXGap;
+    thirdPipeX=canvas.width+pipeXGap*2;
+    fourPipeX=canvas.width+pipeXGap*3;
     firstPipeY=Math.random()*(canvas.height-pipeGap*2)+pipeGap+5;
     secondPipeY=Math.random()*(canvas.height-pipeGap*2)+pipeGap+5;
     thirdPipeY=Math.random()*(canvas.height-pipeGap*2)+pipeGap+5;
@@ -27,41 +41,358 @@ window.onload = function(){
     birdX=canvas.width/3;
     birdY=canvas.height/2;
     firstPipeScore=(firstPipeX-birdX+pipeWidth)/2+bird.width;
-    alsoPipeScore=(secondPipeX-firstPipeX)/2+bird.width;
+    alsoPipeScore=(pipeXGap+bird.width)/2;
+    
 
-    leaderboard.addEventListener('click',showLeaders);
+    leaderButton.addEventListener('click',showLeaders);
     settings.addEventListener('click',showSettings);
     restart.addEventListener('click',restartGame);
-    window.addEventListener('load',game());
+
+    leaderButton2.addEventListener('click',showLeaders2);
+    settings2.addEventListener('click',showSettings2);
+    restart2.addEventListener('click',restartGame);
+
+    canvas.addEventListener('click', (event) => {
+        const x = event.clientX - canvas.offsetLeft;
+        const y = event.clientY - canvas.offsetTop;
+        if (x >= 0 && x < canvas.width && y >= 0 && y < canvas.height) {
+          velocity = approach;
+        }
+    });
+    const now = new Date();
+    const hours=now.getHours();
+    if (hours >= 19 || hours<=8) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        backgroundImage.src = '/img/background-night-big.jpg';
+    }
+    tagName=localStorage.getItem('tagName');
+    difficultyChange();
+    if(localStorage.getItem('theme')==undefined){
+        localStorage.setItem('theme', 'light');
+    }
+    document.querySelector('html').setAttribute('data-theme',localStorage.getItem('theme'));
 }
 
-function compareNumbers(a, b) {
-    return a - b;
+function difficultyChange(){
+    switch(localStorage.getItem('difficulty')){
+        case 'easy':
+            pipeGap=150;
+            pipeXGap=350;
+            acceleration=0.15;
+            pipeSpeed=1.5;
+            approach=-6;
+            break;
+        case 'hard':
+            pipeGap=100;
+            pipeXGap=250;
+            acceleration=0.25;
+            pipeSpeed=2.5;
+            approach=-8;
+            break;
+        default:
+            pipeGap=120;
+            pipeXGap=300;
+            acceleration=0.2;
+            pipeSpeed=2;
+            approach=-7;
+            break;
+    }
 }
 
 function showLeaders(){
-    leaderboardMassive.sort(function(a,b){return b.score-a.score});
-    const listOfLeaders=document.createElement('div');
-    listOfLeaders.classList.add('leaders');
+    leaderboard.style.zIndex=2;
+    pauseWindow.style.zIndex=-1;
+    const leaderBoardMassive=JSON.parse(localStorage.getItem('leaderboard'));
+    leaderBoardMassive.sort(function(a,b){return b.score-a.score});
     const list=document.createElement('ol');
-    listOfLeaders.appendChild(list);
-    for(let i=0;i<leaderboardMassive.length;i++){
+    const exit =document.createElement('button');
+    exit.textContent='Ok';
+    exit.addEventListener('click',()=>{
+        leaderboard.style.zIndex=-1;
+        pauseWindow.style.zIndex=2;
+        leaderboard.innerHTML='';
+    });
+    leaderboard.appendChild(list);
+    const title=document.createElement('li');
+    title.textContent='TOP 10 Players';
+    title.classList.add('title');
+    list.append(title);
+    for(let i=0;i<Math.min(leaderBoardMassive.length,10);i++){
         const leader=document.createElement('li');
+        const tn=document.createElement('a');
+        const diff=document.createElement('a');
+        const sc=document.createElement('a');
+        tn.classList.add('tagName');
+        tn.textContent=leaderBoardMassive[i].tagName;
+        diff.classList.add('difficulty');
+        diff.textContent=leaderBoardMassive[i].difficultyForLeader;
+        sc.classList.add('score');
+        sc.textContent=leaderBoardMassive[i].score;
+        leader.append(tn,diff,sc);
         leader.classList.add('leader');
-        leader.textContent=leaderboardMassive[i].tagName+": "+leaderboardMassive[i].score;
+        list.appendChild(leader);
     }
-    console.log('a');
+    leaderboard.appendChild(exit);
+}
+function showLeaders2(){
+    leaderboard.style.zIndex=3;
+    helloWindow.style.zIndex=-1;
+    const leaderBoardMassive=JSON.parse(localStorage.getItem('leaderboard'));
+    leaderBoardMassive.sort(function(a,b){return b.score-a.score});
+    const list=document.createElement('ol');
+    const exit =document.createElement('button');
+    exit.textContent='Ok';
+    exit.addEventListener('click',()=>{
+        leaderboard.style.zIndex=-1;
+        helloWindow.style.zIndex=3;
+        leaderboard.innerHTML='';
+    });
+    leaderboard.appendChild(list);
+    const title=document.createElement('li');
+    title.textContent='TOP 10 Players';
+    title.classList.add('title');
+    list.append(title);
+    for(let i=0;i<Math.min(leaderBoardMassive.length,10);i++){
+        const leader=document.createElement('li');
+        const tn=document.createElement('a');
+        const diff=document.createElement('a');
+        const sc=document.createElement('a');
+        tn.classList.add('tagName');
+        tn.textContent=leaderBoardMassive[i].tagName;
+        diff.classList.add('difficulty');
+        diff.textContent=leaderBoardMassive[i].difficultyForLeader;
+        sc.classList.add('score');
+        sc.textContent=leaderBoardMassive[i].score;
+        leader.append(tn,diff,sc);
+        leader.classList.add('leader');
+        list.appendChild(leader);
+    }
+    leaderboard.appendChild(exit);
 }
 
 function showSettings(){
-    console.log('b');
+    settingsWindow.style.zIndex=2;
+    pauseWindow.style.zIndex=-1;
+    const theme=document.createElement('div');
+    const themeLight = document.createElement('div');
+    const themeDark = document.createElement('div');
+    themeLight.classList.add('themeLight');
+    themeDark.classList.add('themeDark');
+    themeLight.innerHTML='Switch to light theme';
+    themeDark.innerHTML='Switch to dark theme';
+    if(localStorage.getItem('theme')=='light'){
+        themeLight.classList.add('choised');
+    }
+    else{
+        themeDark.classList.add('choised');
+    }
+    themeLight.addEventListener('click',()=>{
+        themeDark.classList.remove('choised');
+        themeLight.classList.add('choised');
+        localStorage.setItem('theme', 'light');
+        document.querySelector('html').setAttribute('data-theme',localStorage.getItem('theme'));
+        backgroundImage.src = '/img/background-day-big.jpg';
+    });
+    themeDark.addEventListener('click',()=>{
+        themeLight.classList.remove('choised');
+        themeDark.classList.add('choised');
+        localStorage.setItem('theme', 'dark');
+        document.querySelector('html').setAttribute('data-theme',localStorage.getItem('theme'));
+        backgroundImage.src = '/img/background-night-big.jpg';
+    });
+    theme.classList.add('theme');
+    theme.appendChild(themeLight);
+    theme.appendChild(themeDark);
+    const exit =document.createElement('button');
+    exit.textContent='Save';
+    exit.addEventListener('click',()=>{
+        pauseWindow.style.zIndex=2;
+        settingsWindow.style.zIndex=-1;
+        settingsWindow.innerHTML='';
+    });
+    const playerTagName=document.createElement('input');
+    playerTagName.type='text';
+    playerTagName.name='tagName';
+    playerTagName.value=tagName;
+    playerTagName.addEventListener('change',()=>{
+        localStorage.setItem('tagName',playerTagName.value);
+        tagName=localStorage.getItem('tagName');
+    });
+    const tagNameLabel=document.createElement('label');
+    tagNameLabel.textContent='Your Name: ';
+    tagNameLabel.setAttribute('for','tagName');
+    const playerInfoDiv=document.createElement('div');
+    playerInfoDiv.classList.add('playerInfo')
+    playerInfoDiv.appendChild(tagNameLabel);
+    playerInfoDiv.appendChild(playerTagName);
+    const difficulty=document.createElement('div');
+    difficulty.classList.add('difficulty');
+    const levelOne=document.createElement('div');
+    levelOne.classList.add('levelOne');
+    levelOne.textContent='Easy';
+    levelOne.addEventListener('click',()=>{
+        levelOne.classList.add('choised');
+        levelTwo.classList.remove('choised');
+        levelThree.classList.remove('choised');
+        localStorage.setItem('difficulty','easy');
+        difficultyChange();
+    });
+    const levelTwo=document.createElement('div');
+    levelTwo.classList.add('levelTwo');
+    levelTwo.textContent='Normal';
+    levelTwo.addEventListener('click',()=>{
+        levelOne.classList.remove('choised');
+        levelTwo.classList.add('choised');
+        levelThree.classList.remove('choised');
+        localStorage.setItem('difficulty','normal');
+        difficultyChange();
+    });
+    const levelThree=document.createElement('div');
+    levelThree.classList.add('levelThree');
+    levelThree.textContent='Hard';
+    levelThree.addEventListener('click',()=>{
+        levelOne.classList.remove('choised');
+        levelTwo.classList.remove('choised');
+        levelThree.classList.add('choised');
+        localStorage.setItem('difficulty','hard');
+        difficultyChange();
+    });
+    switch(localStorage.getItem('difficulty')){
+        case 'easy':
+            levelOne.classList.add('choised');
+            break;
+        case 'hard':
+            levelThree.classList.add('choised');
+            break;
+        default:
+            levelTwo.classList.add('choised');
+            break;
+    }
+    difficulty.appendChild(levelOne);
+    difficulty.appendChild(levelTwo);
+    difficulty.appendChild(levelThree);
+    settingsWindow.appendChild(playerInfoDiv);
+    settingsWindow.appendChild(difficulty);
+    settingsWindow.appendChild(theme);
+    settingsWindow.appendChild(exit);
 }
 
+function showSettings2(){
+    settingsWindow.style.zIndex=3;
+    helloWindow.style.zIndex=-1;
+    const theme=document.createElement('div');
+    const themeLight = document.createElement('div');
+    const themeDark = document.createElement('div');
+    themeLight.classList.add('themeLight');
+    themeLight.innerHTML='Switch to light theme';
+    themeDark.innerHTML='Switch to dark theme';
+    themeDark.classList.add('themeDark');
+    if(localStorage.getItem('theme')=='light'){
+        themeLight.classList.add('choised');
+    }
+    else{
+        themeDark.classList.add('choised');
+    }
+    themeLight.addEventListener('click',()=>{
+        themeDark.classList.remove('choised');
+        themeLight.classList.add('choised');
+        localStorage.setItem('theme', 'light');
+        document.querySelector('html').setAttribute('data-theme',localStorage.getItem('theme'));
+        backgroundImage.src = '/img/background-day-big.jpg';
+    });
+    themeDark.addEventListener('click',()=>{
+        themeLight.classList.remove('choised');
+        themeDark.classList.add('choised');
+        localStorage.setItem('theme', 'dark');
+        document.querySelector('html').setAttribute('data-theme',localStorage.getItem('theme'));
+        backgroundImage.src = '/img/background-night-big.jpg';
+    });
+    theme.classList.add('theme');
+    theme.appendChild(themeLight);
+    theme.appendChild(themeDark);
+    const exit =document.createElement('button');
+    exit.textContent='Save';
+    exit.addEventListener('click',()=>{
+        helloWindow.style.zIndex=3;
+        settingsWindow.style.zIndex=-1;
+        settingsWindow.innerHTML='';
+    });
+    const playerTagName=document.createElement('input');
+    playerTagName.type='text';
+    playerTagName.name='tagName';
+    playerTagName.value=tagName;
+    playerTagName.addEventListener('change',()=>{
+        localStorage.setItem('tagName',playerTagName.value);
+        tagName=localStorage.getItem('tagName');
+    });
+    const tagNameLabel=document.createElement('label');
+    tagNameLabel.textContent='Your Name: ';
+    tagNameLabel.setAttribute('for','tagName');
+    const playerInfoDiv=document.createElement('div');
+    playerInfoDiv.classList.add('playerInfo')
+    playerInfoDiv.appendChild(tagNameLabel);
+    playerInfoDiv.appendChild(playerTagName);
+    const difficulty=document.createElement('div');
+    difficulty.classList.add('difficulty');
+    const levelOne=document.createElement('div');
+    levelOne.classList.add('levelOne');
+    levelOne.textContent='Easy';
+    levelOne.addEventListener('click',()=>{
+        levelOne.classList.add('choised');
+        levelTwo.classList.remove('choised');
+        levelThree.classList.remove('choised');
+        localStorage.setItem('difficulty','easy');
+        difficultyChange();
+    });
+    const levelTwo=document.createElement('div');
+    levelTwo.classList.add('levelTwo');
+    levelTwo.textContent='Normal';
+    levelTwo.addEventListener('click',()=>{
+        levelOne.classList.remove('choised');
+        levelTwo.classList.add('choised');
+        levelThree.classList.remove('choised');
+        localStorage.setItem('difficulty','normal');
+        difficultyChange();
+    });
+    const levelThree=document.createElement('div');
+    levelThree.classList.add('levelThree');
+    levelThree.textContent='Hard';
+    levelThree.addEventListener('click',()=>{
+        levelOne.classList.remove('choised');
+        levelTwo.classList.remove('choised');
+        levelThree.classList.add('choised');
+        localStorage.setItem('difficulty','hard');
+        difficultyChange();
+    });
+    switch(localStorage.getItem('difficulty')){
+        case 'easy':
+            levelOne.classList.add('choised');
+            break;
+        case 'hard':
+            levelThree.classList.add('choised');
+            break;
+        default:
+            levelTwo.classList.add('choised');
+            break;
+    }
+    difficulty.appendChild(levelOne);
+    difficulty.appendChild(levelTwo);
+    difficulty.appendChild(levelThree);
+    settingsWindow.appendChild(playerInfoDiv);
+    settingsWindow.appendChild(difficulty);
+    settingsWindow.appendChild(theme);
+    settingsWindow.appendChild(exit);
+}
 function restartGame(){
+    pauseWindow.style.zIndex=-1;
+    helloWindow.style.zIndex=-1;
+    scoreInput.innerHTML=scoreInput.textContent.slice(0,12)+Number(score);
     velocity=0, score=0;
-    scoreInput.innerHTML=score;
     birdY=canvas.height/2;
-    firstPipeX=900,secondPipeX=1200, thirdPipeX=1500,fourPipeX=1800;
+    firstPipeX=canvas.width;
+    secondPipeX=canvas.width+300;
+    thirdPipeX=canvas.width+600;
+    fourPipeX=canvas.width+900;
     mainWindow.style.zIndex=2;
     mainWindow.classList.remove('blured');
     game();
@@ -70,10 +401,11 @@ function restartGame(){
 document.body.addEventListener('keydown',(event)=>{
     event.stopPropagation();
 
-    if(event.code=='Space'){//change for mobile also
+    if(event.code=='Space'){
         velocity=approach;
     }
 });
+
 
 function drawPipe(pipeX,pipeY){
     context.drawImage(pipeTop,pipeX,-pipeGap,pipeWidth,pipeY);
@@ -243,16 +575,26 @@ function impact(){
 }
 
 function game(){
+    context.mozImageSmoothingEnabled = false;
+    context.webkitImageSmoothingEnabled = false;
+    context.msImageSmoothingEnabled = false;
+    context.imageSmoothingEnabled = false;
     context.clearRect(0,0,canvas.width,canvas.height);
     context.drawImage(backgroundImage,0,0,canvas.width,canvas.height);
     context.drawImage(bird,birdX,birdY);
-
+    console.log(score);
     drawPipes();
 
     if(impact()){
         mainWindow.style.zIndex=1;
+        pauseWindow.style.zIndex=2;
         mainWindow.classList.add('blured');
-        leaderboardMassive.push({name: tagName,score: score});
+        const difficultyForLeader=localStorage.getItem('difficulty');
+        struct.push({tagName,score,difficultyForLeader});
+        localStorage.setItem('leaderboard', JSON.stringify(struct));
+        scoreInput.innerHTML=scoreInput.textContent.slice(0,scoreInput.textContent.length-1)+Number(score);
+        score=0;
+        console.clear();
         return
     }
     
@@ -260,17 +602,15 @@ function game(){
     if(score==0&&scoreTmp==firstPipeScore){
         scoreTmp=0;
         score++;
-        scoreInput.innerHTML=score;
+        
     }
     if(score>0&&scoreTmp==alsoPipeScore){
         scoreTmp=0;
         score++;
-        scoreInput.innerHTML=score;
     }
 
     velocity+=acceleration;
     birdY+=velocity;
-
 
     requestAnimationFrame(game);
 }
